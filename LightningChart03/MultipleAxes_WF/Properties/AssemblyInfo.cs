631e7fb2acbe81e78966bb1cdbfdebf53dd173e6
ArctionLtd <nikolai.arsenov@arction.com>
2017-01-16 15:52:46 +0200

    3rd tutorial is added to repository. Multiple axes example.
    - Windows Forms project with LightningChart API
    - WPF project with Non-Bindable LightningChart API
    - WPF project with Semi-Bindable LightningChart API

---
+++ LightningChart03/MultipleAxes_WF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MultipleAxes_WF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Arction Ltd")]
+[assembly: AssemblyProduct("MultipleAxes_WF")]
+[assembly: AssemblyCopyright("©  Arction Ltd 2017")]
+[assembly: AssemblyTrademark("LightningChart®")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b259addb-4202-4b0f-a453-14547901672a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
